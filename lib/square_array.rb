0001838bd4970b9f690c05dcb90d51edb7215ddf
adept-bell-4492 <garlandegoma@gmail.com>
2020-07-03 01:49:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
+
 def square_array(array)
+  count= 0 
+  new_array= [ ]
+  while array[count] do
+    puts Math.sqrt(array[count])
+    count += 1 
+  end
+  new_array
+    
   # your code here
 end
